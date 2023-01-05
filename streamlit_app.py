69748d5edcd8976c2f94fbe3ec0e21f5927ab1dd
johannes117 <51395795+johannes117@users.noreply.github.com>
2023-01-05 21:01:49 +1100

    working with stable diffusion api

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,33 @@ if uploaded_file is not None:
     st.image(image, caption="Original Image", use_column_width=True)
 
     # Send the image to the Stable Diffusion API
-    api_key = "your-api-key"
-    api_url = "https://api.stablediffusion.com/v1/room"
-    headers = {"Authorization": f"Bearer {api_key}"}
-    files = {"image": uploaded_file}
-    response = requests.post(api_url, headers=headers, files=files)
+    api_key = "76XgHJI9sRqRf24mQIVe6KySJZyDV6wcJbhzVVkYamRW2yie9UMTovBnyvu7"
+    api_url = "https://stablediffusionapi.com/api/v3/img2img"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    body = {
+        "key": "76XgHJI9sRqRf24mQIVe6KySJZyDV6wcJbhzVVkYamRW2yie9UMTovBnyvu7",
+        "prompt": "high resolution photography interior design, wooden floor, bedroom cozy atmosphere",
+        "negative_prompt": "null",
+        "init_image": "https://d12mivgeuoigbq.cloudfront.net/assets/blog/Great%20American%20Home%20Store/greatamericanhomestore-seotool-56745-essentialbedroomfurniture-blogbanner1.jpg",
+        "width": "512",
+        "height": "512",
+        "samples": "1",
+        "num_inference_steps": "30",
+        "guidance_scale": 7.5,
+        "strength": 0.7,
+        "seed": "null",
+        "webhook": "null",
+        "track_id": "null"
+    }
+    response = requests.post(api_url, headers=headers, json=body)
 
     # Display the staged image
     if response.status_code == 200:
-        staged_image_url = response.json()["data"]["url"]
-        staged_image = Image.open(requests.get(staged_image_url, stream=True).raw)
-        st.image(staged_image, caption="Staged Image", use_column_width=True)
+        st.write(response.json())
+        # staged_image_url = response.json()["output"][0]
+        # staged_image = Image.open(requests.get(staged_image_url, stream=True).raw)
+        # st.image(staged_image, caption="Staged Image", use_column_width=True)
     else:
         st.error("There was an error processing the image")
